7bb218d9ac9e3cd8c6f617afe2579be3d51732e0
dcv <dcvtrash@gmail.com>
2020-07-21 19:24:46 +0200

    change type to worker

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config();
 
+// log in as rin
+client.login(process.env.TOKEN);
+
+// hello message
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// commands i guess
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
   }
 });
-
-client.login(process.env.TOKEN);
