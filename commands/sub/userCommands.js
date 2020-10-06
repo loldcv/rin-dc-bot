2b2a6ea0e703d35d97797ee4bfd754781f234cc1
dcv <dcvtrash@gmail.com>
2020-10-06 19:13:50 +0200

    change avatar command so people dont get mentioned

--- commands/sub/userCommands.js
+++ commands/sub/userCommands.js
@@ -12,9 +12,17 @@ class UserCommands extends CommandGroup {
      * @param {Discord.Message} msg 
      */
     showAvatar(msg) {
-        //console.log(msg.guild.members);
-        if(msg.mentions.members.size > 0) msg.channel.send(msg.mentions.members.first().user.displayAvatarURL());
-        else msg.channel.send(mentionUser(msg) + " I either can't find that user or you did not mention anybody.");
+        if (msg.rin_args.length > 0) {
+            msg.guild.members.fetch({ query: msg.rin_args, limit: 1 })
+                .then(res => {
+                    msg.channel.send(res.entries().next().value[1].user.displayAvatarURL())
+                })
+                .catch(e => {
+                    msg.channel.send(mentionUser(msg) + " I can not find that user.")
+                });
+        } else {
+            msg.channel.send(mentionUser(msg) + " You did not specify a username to look for.")
+        }
     }
 
 }
